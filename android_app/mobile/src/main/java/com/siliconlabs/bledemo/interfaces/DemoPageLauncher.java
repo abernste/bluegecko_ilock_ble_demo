558c407eda7cff1d9d42aa2524fc7af5796fff3b
abernste <amit.bernstein@intel.com>
2019-06-19 15:45:23 +0300

    remove prev android app

--- android_app/mobile/src/main/java/com/siliconlabs/bledemo/interfaces/DemoPageLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.siliconlabs.bledemo.interfaces;
-
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
-
-public interface DemoPageLauncher {
-    DialogFragment launchPage(View view, FragmentManager fragmentManager);
-}
